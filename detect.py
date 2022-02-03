24689c749c12456a44655a5fdb85383c211c5151
achelm15 <achelm@rams.colostate.edu>
2022-02-03 12:47:41 -0700

    image size variable

--- detect.py
+++ detect.py
@@ -15,15 +15,15 @@ if str(ROOT) not in sys.path:
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 
-def process_image(img):
-    image = np.array(img.resize((256, 256)), dtype="int8")
+def process_image(img, imgsz):
+    image = np.array(img.resize((imgsz, imgsz)), dtype="int8")
     #     image /= 255.
     image = np.expand_dims(image, axis=0)
     return image
 
 
-def detect_image(image, interpreter):
-    pimage = process_image(image)
+def detect_image(image, interpreter, imgsz):
+    pimage = process_image(image,imgsz)
     input_index = interpreter.get_input_details()[0]["index"]
     output_index = interpreter.get_output_details()[0]["index"]
     interpreter.set_tensor(input_index, np.array(pimage, dtype="uint8"))
@@ -38,13 +38,13 @@ def detect_image(image, interpreter):
     return outs, time
 
 
-def detect_video(video, interpreter):
+def detect_video(video, interpreter, imgsz):
     vidcap = cv2.VideoCapture(video)
     success, image = vidcap.read()
     count = 0
     time_array = []
     while success and count<20:
-        outs, time = detect_image(Image.fromarray(image), interpreter)
+        outs, time = detect_image(Image.fromarray(image), interpreter, imgsz)
         print(np.array(outs).shape)
         time_array.append(time)
         success, image = vidcap.read()
@@ -56,6 +56,7 @@ def detect_video(video, interpreter):
 def run(weights=ROOT / 'yolov5s.pt', source=ROOT / 'data/images'):
     model_path = opt.weights
     source = opt.source
+    imgsz = opt.imgsz
     interpreter = tflite.Interpreter(model_path)
     interpreter = tflite.Interpreter(
         model_path, experimental_delegates=[tflite.load_delegate("libedgetpu.so.1")]
@@ -69,10 +70,10 @@ def run(weights=ROOT / 'yolov5s.pt', source=ROOT / 'data/images'):
     
     if source.endswith("jpg") or source.endswith("jpeg"):
         source = Image.open(source)
-        detect_image(source, interpreter)
+        detect_image(source, interpreter, imgsz)
     elif source.endswith("m4v") or source.endswith("mp4"): 
         video = "4p1b_01A2.m4v"
-        detect_video(video, interpreter)
+        detect_video(video, interpreter, imgsz)
     else:
         return
 
@@ -85,6 +86,7 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument("--weights", type=str, default=ROOT / "test.tflite", help="model path(s)",)
     parser.add_argument("--source", type=str,default=ROOT / "test.jpg",)
+    parser.add_argument("--imgsz", type=int, default=256)
     opt = parser.parse_args()
     return opt
 
