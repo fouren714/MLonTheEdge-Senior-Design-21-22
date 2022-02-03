611f95543682b86d007cc6c72b0ddf05d2a36f6b
achelm15 <achelm@rams.colostate.edu>
2022-02-02 21:23:10 -0700

    continue testing

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
 import tflite_runtime.interpreter as tflite
-model_path = '../best-int8_edgetpu.tflite'
+import PIL
+import PIL.Image
+import datetime
+
+model_path = "../best-int8_edgetpu.tflite"
 interpreter = tflite.Interpreter(model_path)
-interpreter = tflite.Interpreter(model_path,
-  experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
+interpreter = tflite.Interpreter(
+    model_path, experimental_delegates=[tflite.load_delegate("libedgetpu.so.1")]
+)
 print(type(interpreter))
+
+interpreter.allocate_tensors()
+image = "test.jpg"
+pimage = process_image(Image.open(image))
+print(pimage)
+image = Image.open(image)
+input_index = interpreter.get_input_details()[0]["index"]
+output_index = interpreter.get_output_details()[0]["index"]
+interpreter.set_tensor(input_index, np.array(pimage))
+start = datetime.datetime.now()
+interpreter.invoke()
+print(datetime.datetime.now() - start)
+outs = interpreter.get_tensor(output_index)
+shape = np.array(pimage).shape
+outs = [np.array(outs)]
+image = np.array(image)
+print(image.shape)
